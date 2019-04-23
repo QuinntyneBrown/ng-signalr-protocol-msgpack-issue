d74ee0c71916c07d99e92baa9c6432bf0e1d8641
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2019-04-23 13:13:57 -0400

    inital code to demo error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HubConnectionBuilder } from "@aspnet/signalr";
+import { MessagePackHubProtocol } from "@aspnet/signalr-protocol-msgpack";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-signalr-protocol-msgpack-issue';
+
+  ngOnInit() {
+    var connection = new HubConnectionBuilder()
+    .withUrl("")
+    .withHubProtocol(new MessagePackHubProtocol())
+    .build();
+  }
 }
